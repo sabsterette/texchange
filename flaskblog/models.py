1940733a500a6f1dc865663f31dec32a9f534a66
johnk1729 <36747091+johnk1729@users.noreply.github.com>
2019-03-20 00:24:12 -0400

    Added new attributes to models

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -12,6 +12,7 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(200), unique=True, nullable=False)
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
+    description = db.Column(db.String(255), nullable=True)
     posts = db.relationship('Post', backref='author', lazy=True)
 
     #repr is basically toString
@@ -22,9 +23,12 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    content = db.Column(db.Text, nullable=False)
+    description = db.Column(db.String(255))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    quality = db.Column(db.String(50), nullable=False)
+    class_id = db.Column(db.String(7), nullable=False)
+    edition = db.Column(db.Integer, nullable=False)
 
     def __repr__(self):
         return f"Post('{self.title}', '{self.date_posted}')"
-
